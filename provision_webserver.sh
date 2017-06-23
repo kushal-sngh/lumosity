6cf9c6e393edd3777c18d43af11ebc235077e9d4
kushal <kushalsoft@gmail.com>
2017-06-22 23:15:47 -0600

    Oauth docker coantiner added

--- provision_webserver.sh
+++ provision_webserver.sh
@@ -4,7 +4,7 @@ if [ -d "lumosity" ]; then
   rm -rf lumosity
 fi
 
-git clone https://github.com/kushal-sngh/lumosity.git
+#git clone https://github.com/kushal-sngh/lumosity.git
 
 #Optional commands to.You may need them to re-run script.
  
@@ -16,6 +16,7 @@ docker rm $(docker ps -a -q)
 
 #Delete httpd image if exist
 docker rmi -f kushal/webserver
+docker rmi -f kushal/oauth
 
 # End of optional commands
 
@@ -28,14 +29,21 @@ docker run -d -p 8080 --name webserver kushal/webserver
 #Wait for container is up...
 sleep 3.0
 
-IPADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' webserver)
-#echo $IPADDR
+WEBIPADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' webserver)
+echo "Web Server ${WEBIPADDR}"
 
-#Test
-curl http://${IPADDR}
+#Build new image
+docker build --no-cache=false -t kushal/webserver $(pwd)/oauth
 
-#Local test URL
-#echo "Type this into browser"
-#echo "http://${IPADDR}"
+#Start container from webserver image
+docker run -d -p 8080 --name webserver kushal/oauth
+
+#Wait for container is up...
+sleep 3.0
+
+
+AUTHIPADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' oauth)
+echo "Auth Server ${AUTHIPADDR}"
+
 
  
