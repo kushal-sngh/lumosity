772110be5137b820a3fbd836f806f256e782c69a
kushal <kushalsoft@gmail.com>
2017-06-23 00:04:55 -0600

    callback changed

--- install_oauth2.sh
+++ install_oauth2.sh
@@ -18,4 +18,4 @@ fi
 
 source /etc/profile
 
-oauth2_proxy -client-id="aa4227354123159f6546" -client-secret="877254cf43ef0d9c7a5eadcdb81ed0fd227cf895" -provider="github" -cookie-secret="kushal@##$%-" -email-domain="*" -upstream http://0.0.0.0:80 -redirect-url=http://0.0.0.0:4180/oauth2/callback -cookie-secure=false -http-address=0.0.0.0:4180
+oauth2_proxy -client-id="aa4227354123159f6546" -client-secret="877254cf43ef0d9c7a5eadcdb81ed0fd227cf895" -provider="github" -cookie-secret="kushal@##$%-" -email-domain="*" -upstream http://172.17.0.2 -redirect-url=http://127.0.0.1:4180/oauth2/callback -cookie-secure=false -http-address=0.0.0.0:4180
